78f639d17c8bd897d257dd381d5df8b3892285ed
smartin9111 <sinka.martin.91@gmail.com>
2023-10-30 20:17:18 +0100

    1

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
